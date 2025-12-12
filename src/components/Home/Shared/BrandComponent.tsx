14cf183a503a22f6bc6d05691b06a6eeec865aee
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2025-12-12 11:14:31 +0600

    sovle issue

--- src/components/Home/Shared/BrandComponent.tsx
+++ src/components/Home/Shared/BrandComponent.tsx
@@ -7,19 +7,24 @@ import logo4 from '../../../../public/logo4.png'
 import logo5 from '../../../../public/logo6.png'
 const BrandComponent = () => {
     return (
-        <div className='flex container mx-auto items-center  justify-center '>
-            <div className='flex flex-col md:flex-row items-center gap-[75px]'>
-                <p className='text-[#929292] font-normal text-[16px]'>TRUSTED BY </p>
-                <div className='flex gap-20 flex-wrap justify-center'>
-                    <Image src={logo1} alt="logos" width={900} height={900} className='w-28 h-14 object-contain ' />
+        <div className='flex container mx-auto items-center justify-center'>
+            <div className='flex flex-col md:flex-row items-center justify-center gap-[75px]'>
+
+                <p className='text-[#929292] font-normal text-[16px] text-center md:text-left'>
+                    TRUSTED BY
+                </p>
+
+                <div className='flex gap-20 flex-wrap justify-center items-center'>
+                    <Image src={logo1} alt="logos" width={900} height={900} className='w-28 h-14 object-contain' />
                     <Image src={logo2} alt="logos" width={900} height={900} className='w-24 h-14 object-contain' />
                     <Image src={logo3} alt="logos" width={900} height={900} className='w-24 h-16 object-contain' />
-                    <Image src={logo4} alt="logos" width={900} height={900} className='w-16 h-14 ' />
-                    {/* <Image src="/logo5.png" alt="logos" width={900} height={900} className='w-16 h-14 ' /> */}
-                    <Image src={logo5} alt="logos" width={900} height={900} className='w-28 h-12 ' />
+                    <Image src={logo4} alt="logos" width={900} height={900} className='w-16 h-14 object-contain' />
+                    <Image src={logo5} alt="logos" width={900} height={900} className='w-28 h-6 object-contain' />
                 </div>
+
             </div>
         </div>
+
     )
 }
 
