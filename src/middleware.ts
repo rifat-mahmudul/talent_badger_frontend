07e503a00d5e5afb91568003dadbdd8bb89c2eec
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2025-12-12 12:28:44 +0600

    solve

--- src/middleware.ts
+++ src/middleware.ts
@@ -38,37 +38,78 @@
 // };
 
 
-import { getToken } from "next-auth/jwt";
-import { NextResponse, NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse, NextRequest } from "next/server";
 
-  const url = request.nextUrl;
+// export async function middleware(request: NextRequest) {
+//   const token = await getToken({
+//     req: request,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
 
-  // If no token → redirect
-  if (!token) {
-    return NextResponse.redirect(new URL("/", request.url));
+//   const url = request.nextUrl;
+
+//   // If no token → redirect
+//   if (!token) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+
+//   const role = token.role as string;
+
+//   // Engineer protected routes
+//   if (url.pathname.startsWith("/engineer") && role !== "engineer") {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+
+//   // Account protected routes (normal users)
+//   if (url.pathname.startsWith("/account") && !["user", "client"].includes(role)) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/account/:path*", "/engineer/:path*"],
+// };
+
+
+
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+
+  function middleware(req) {
+    const token = req.nextauth.token;
+    const role = token?.role;
+    const path = req.nextUrl.pathname;
+ 
+
+    // User dashboard protection
+    if (path.startsWith("/user") && role !== "user") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    // Contractor/Service Provider dashboard protection
+    if (path.startsWith("/engineer") && role !== "engineer") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+     
+      authorized: ({ token }) => !!token,
+    },
   }
-
-  const role = token.role as string;
-
-  // Engineer protected routes
-  if (url.pathname.startsWith("/engineer") && role !== "engineer") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  // Account protected routes (normal users)
-  if (url.pathname.startsWith("/account") && !["user", "client"].includes(role)) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  return NextResponse.next();
-}
+);
 
 export const config = {
-  matcher: ["/account/:path*", "/engineer/:path*"],
+  matcher: [
+    "/account/:path*",
+    "/engineer:path*",
+  ],
 };
